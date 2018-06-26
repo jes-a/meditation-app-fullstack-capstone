96fb047b247539dad6680979cd2e99d5d2121f0d
Jes Albro <jesalbro@gmail.com>
2018-06-25 22:34:53 -0700

    Add document ready function to client.js

--- public/client.js
+++ public/client.js
@@ -1,1 +1,16 @@
 "use strict";
+
+
+// ----------- DOCUMENT READY FUNCTION ---------------------
+
+$(document).ready(function() {
+    $('#landing-screen').show();
+    $('#login-screen').hide();
+    $('#signup-screen').hide();
+    $('#site-nav').hide();
+    $('#dashboard-screen').hide();
+    $('#add-session-screen').hide();
+    $('#journal-screen').hide();
+    $('#change-password-screen').hide();
+    $('#footer').hide();    
+});
